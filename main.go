e6df4d381ec522edba53ad23f3841c9aaed832df
Saji Laptop <yoyoguy111@gmail.com>
2022-02-21 22:46:17 -0600

    added doodle

--- main.go
+++ main.go
@@ -9,11 +9,35 @@ import (
 	_ "github.com/heroku/x/hmetrics/onload"
 )
 
+func serveHome(c *gin.Context) {
+	log.Println(c.Request.URL)
+	if c.Request.URL.Path != "/" {
+		http.Error(c.Writer, "Not found", http.StatusNotFound)
+		return
+	}
+	if c.Request.Method != http.MethodGet {
+		http.Error(c.Writer, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	http.ServeFile(c.Writer, c.Request, "index.html")
+}
 func main() {
 	port := os.Getenv("PORT")
 
 	if port == "" {
-		log.Fatal("$PORT must be set")
+		//log.Fatal("$PORT must be set")
+		router := gin.New()
+		router.Use(gin.Logger())
+		router.LoadHTMLGlob("templates/*.tmpl.html")
+		router.Static("/static", "static")
+		hub := newHub()
+		go hub.run()
+		router.GET("/", serveHome) //.HandleFunc("/", serveHome)
+		router.GET("/ws", func(c *gin.Context) {
+			serveWs(hub, c.Writer, c.Request)
+		})
+
+		router.Run(":" + "8000")
 	}
 
 	router := gin.New()
